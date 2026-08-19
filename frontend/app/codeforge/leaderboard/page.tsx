42ed273d66af8a8768cf57bbe5851544c36e67e6
Abhyudai Tiwari <tiwaripreeti7586@gmail.com>
2026-08-19 13:01:31 +0530

    feat: add CodeForge leaderboard route

---
+++ frontend/app/codeforge/leaderboard/page.tsx
@@ -0,0 +1,2 @@
+import MainLayout from '@/components/codeforge/MainLayout';import GlobalLeaderboardPage from '@/components/codeforge/GlobalLeaderboardPage';
+export default function CodeForgeLeaderboardRoute(){return <MainLayout title="Leaderboard"><GlobalLeaderboardPage/></MainLayout>}
